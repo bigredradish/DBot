2194de7ea40da96218e9ba93a41adc09d2d69140
David Radford <dave@bigredradish.com>
2018-01-29 16:09:58 +0000

    Create xmas.py

---
+++ python/xmas.py
@@ -0,0 +1,11 @@
+import datetime
+import time
+
+#currentYear = datetime.now().year
+while True:
+    xmas = datetime.datetime(2018, 12, 25) - datetime.datetime.now()
+    #xmas = datetime.datetime(currentYear, 12, 25) - datetime.datetime.now()
+    daysleft = xmas.days
+    hoursleft = xmas.seconds/3600
+
+    print("Ho ho ho! It's %i days and %i hours until xmas!" %(daysleft, hoursleft))
